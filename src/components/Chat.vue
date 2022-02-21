eebdad8540bf3724b98cd863818369b6538cacc9
Martin Ott <pahaloom@gmail.com>
2022-02-21 16:02:12 +0200

    FB Like

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,17 +1,5 @@
-<template lang="html">
+<template>
     <div>
-        <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fwww.facebook.com%2FRyytliKelder&layout=button_count&size=small&width=89&height=20&appId" width="89" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        <iframe src='https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FRyytliKelder&width=450&layout=standard&action=like&size=large&share=true&height=35&appId' width="450" height="35" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
     </div>
 </template>
-<script>
-export default {}
-</script>
-<style>
-.fb {
-    color:#35495E;
-    font-weight: 800;
-    font-size: .5em;
-    margin-top: -0.15em;
-}
-
-</style>
